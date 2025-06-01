63cb4d0ad133f745745f3e70cdce25c32a09db5a
scribencode <deyeye99@gmail.com>
2025-06-01 08:21:15 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+fetch('/data.json')
+.then(res =>{
+    if(!res.ok){
+        throw new Error('There was an error loading json')
+    }
+    return res.json()
+})
+.then(data =>{
+    const countries = data
+       document.querySelector('.wrapper').innerHTML = countries.map(country =>
+        `        <article>
+            <img src= ${country.flags.png} alt="" srcset="">
+            <p>Population: ${country.population}</p>
+            <p>Region:${country.region}</p>
+            <p>Capital:${country.capital}</p>
+        </article>
+        `
+    ).join('')
+
+})
+.catch(error => console.error('error fetching data',error))
+
+
+
