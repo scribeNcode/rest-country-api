5561220c5a3f5367a368f3f22656e3c948234481
scribencode <deyeye99@gmail.com>
2025-06-02 09:32:39 +0100

    add logic to display and search countries

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-
-
-fetch('/data.json')
-.then(res =>{
-    if(!res.ok){
-        throw new Error('There was an error loading json')
+fetch("/data.json")
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("There was an error loading json");
     }
-    return res.json()
-})
-.then(data =>{
-    const countries = data
-       document.querySelector('.all-countries-wrapper').innerHTML = countries.map(country =>
-        `        <article>
+    return res.json();
+  })
+  .then((data) => {
+    const allCountryWrapper = document.querySelector(".all-countries-wrapper");
+    const countries = data;
+    renderCountries(countries);
+
+    // userSerchInput tag
+    const userInput = document.querySelector("#userSerchInput");
+    userInput.addEventListener("input", () => {
+      const query = userInput.value.toLowerCase().trim();
+
+      if (userInput === "") {
+        renderCountries(countries);
+      } else {
+        // filter based on user input
+        const filtered = countries.filter((country) =>
+          country.name.toLowerCase().includes(query)
+        );
+        renderCountries(filtered);
+      }
+    });
+
+    function renderCountries(allCountry) {
+      allCountryWrapper.innerHTML = allCountry
+        .map(
+          (country) =>
+            `        <article>
             <img src= ${country.flags.png} alt="" srcset="">
             <p>Population: ${country.population}</p>
             <p>Region:${country.region}</p>
             <p>Capital:${country.capital}</p>
         </article>
         `
-    ).join('')
-
-})
-.catch(error => console.error('error fetching data',error))
-
-
-
+        )
+        .join("");
+    }
+  })
+  .catch((error) => console.error("error fetching data", error));
